259c5b4592446f8cce20e672f2f98336d22f94bd
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2025-11-08 19:28:32 +0200

    feat: implement CRUD operations for the Product entity

--- src/main/java/com/cosmocats/marketplace/domain/Product.java
+++ src/main/java/com/cosmocats/marketplace/domain/Product.java
@@ -3,12 +3,14 @@ package com.cosmocats.marketplace.domain;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Entity
 @Data
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
 
     @Column(nullable = false)
     private String name;
