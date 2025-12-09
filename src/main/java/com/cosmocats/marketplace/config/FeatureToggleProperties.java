044539ab4124be7d669fc15da1d0bd9f99eee8f7
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2025-12-09 18:12:30 +0200

    feat: implement feature toggle

---
+++ src/main/java/com/cosmocats/marketplace/config/FeatureToggleProperties.java
@@ -0,0 +1,20 @@
+package com.cosmocats.marketplace.config;
+
+import java.util.Map;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@NoArgsConstructor
+@Configuration
+@ConfigurationProperties(prefix = "feature")
+public class FeatureToggleProperties {
+
+    Map<String, Boolean> toggles;
+
+    public boolean check(String featureToggle){
+        return toggles.getOrDefault(featureToggle, false);
+    }
+}
---
+++ src/main/java/com/cosmocats/marketplace/featuretoggle/aspect/FeatureToggleAspect.java
@@ -0,0 +1,37 @@
+package com.cosmocats.marketplace.featuretoggle.aspect;
+
+import com.cosmocats.marketplace.featuretoggle.FeatureToggleService;
+import com.cosmocats.marketplace.featuretoggle.FeatureToggles;
+import com.cosmocats.marketplace.featuretoggle.annotation.FeatureToggle;
+import com.cosmocats.marketplace.featuretoggle.exception.FeatureToggleNotEnabledException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class FeatureToggleAspect {
+
+    private final FeatureToggleService featureToggleService;
+
+    @Around(value = "@annotation(featureToggle)")
+    public Object checkFeatureToggleAnnotation(ProceedingJoinPoint joinPoint, FeatureToggle featureToggle) throws Throwable {
+        return checkToggle(joinPoint, featureToggle);
+    }
+
+    private Object checkToggle(ProceedingJoinPoint joinPoint, FeatureToggle featureToggle) throws Throwable {
+        FeatureToggles toggle = featureToggle.value();
+
+        if (featureToggleService.check(toggle.getFeatureName())) {
+            return joinPoint.proceed();
+        }
+        log.warn("Feature toggle {} is not enabled!", toggle.getFeatureName());
+        throw new FeatureToggleNotEnabledException(toggle.getFeatureName());
+    }
+
+}
